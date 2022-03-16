b6c8c1e82cc98a99c71eb34b80db0fb72f97c536
MoukthikaSomaraju <ms2954@njit.edu>
2022-03-16 13:24:27 -0400

    .

--- app/src/main/java/com/example/healthapp/MainActivity.java
+++ app/src/main/java/com/example/healthapp/MainActivity.java
@@ -1,14 +1,33 @@
-package com.example.healthapp;
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+        package="com.samsung.android.app.stepdiary"
+        android:versionCode="1"
+        android:versionName="1.0" >
 
-import androidx.appcompat.app.AppCompatActivity;
+<uses-permission android:name="android.permission.INTERNET" />
 
-import android.os.Bundle;
+<queries>
+<package android:name="com.sec.android.app.shealth" />
+</queries>
 
-public class MainActivity extends AppCompatActivity {
+<application
+        android:allowBackup="true"
+                android:icon="@drawable/ic_launcher"
+                android:label="@string/app_name"
+                android:theme="@style/AppTheme" >
+<meta-data
+        android:name="com.samsung.android.health.permission.read"
+        android:value="com.samsung.health.step_count;com.samsung.shealth.step_daily_trend" />
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+<activity
+            android:name=".StepDiaryActivity"
+                    android:label="@string/app_name"
+                    android:screenOrientation="portrait" >
+<intent-filter>
+<action android:name="android.intent.action.MAIN" />
+
+<category android:name="android.intent.category.LAUNCHER" />
+</intent-filter>
+</activity>
+</application>
+
+</manifest>
