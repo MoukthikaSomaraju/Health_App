46e09e022f0c0ce2dd5b891074ea705915e8d5ee
MoukthikaSomaraju <ms2954@njit.edu>
2022-03-16 13:26:52 -0400

    .

--- app/src/main/java/com/example/healthapp/MainActivity.java
+++ app/src/main/java/com/example/healthapp/MainActivity.java
@@ -1,33 +1,198 @@
-<manifest xmlns:android="http://schemas.android.com/apk/res/android"
-        package="com.samsung.android.app.stepdiary"
-        android:versionCode="1"
-        android:versionName="1.0" >
 
-<uses-permission android:name="android.permission.INTERNET" />
 
-<queries>
-<package android:name="com.sec.android.app.shealth" />
-</queries>
+package com.samsung.android.simplehealth;
 
-<application
-        android:allowBackup="true"
-                android:icon="@drawable/ic_launcher"
-                android:label="@string/app_name"
-                android:theme="@style/AppTheme" >
-<meta-data
-        android:name="com.samsung.android.health.permission.read"
-        android:value="com.samsung.health.step_count;com.samsung.shealth.step_daily_trend" />
+import com.samsung.android.sdk.healthdata.HealthConnectionErrorResult;
+import com.samsung.android.sdk.healthdata.HealthConstants.StepCount;
+import com.samsung.android.sdk.healthdata.HealthDataStore;
+import com.samsung.android.sdk.healthdata.HealthPermissionManager;
+import com.samsung.android.sdk.healthdata.HealthPermissionManager.PermissionKey;
+import com.samsung.android.sdk.healthdata.HealthPermissionManager.PermissionType;
+import com.samsung.android.simplehealth.databinding.ActivityMainBinding;
 
-<activity
-            android:name=".StepDiaryActivity"
-                    android:label="@string/app_name"
-                    android:screenOrientation="portrait" >
-<intent-filter>
-<action android:name="android.intent.action.MAIN" />
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.Menu;
 
-<category android:name="android.intent.category.LAUNCHER" />
-</intent-filter>
-</activity>
-</application>
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
-</manifest>
+import java.util.Collections;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String APP_TAG = "SimpleHealth";
+
+    private ActivityMainBinding mBinding;
+
+    private HealthDataStore mStore;
+    private StepCountReporter mReporter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        // Create a HealthDataStore instance and set its listener
+        mStore = new HealthDataStore(this, mConnectionListener);
+        // Request the connection to the health data store
+        mStore.connectService();
+    }
+
+    @Override
+    public void onDestroy() {
+        mReporter.stop();
+        mStore.disconnectService();
+        super.onDestroy();
+    }
+
+    private final HealthDataStore.ConnectionListener mConnectionListener = new HealthDataStore.ConnectionListener() {
+
+        @Override
+        public void onConnected() {
+            Log.d(APP_TAG, "Health data service is connected.");
+            mReporter = new StepCountReporter(mStore, mStepCountObserver, new Handler(Looper.getMainLooper()));
+            if (isPermissionAcquired()) {
+                mReporter.start();
+            } else {
+                requestPermission();
+            }
+        }
+
+        @Override
+        public void onConnectionFailed(HealthConnectionErrorResult error) {
+            Log.d(APP_TAG, "Health data service is not available.");
+            showConnectionFailureDialog(error);
+        }
+
+        @Override
+        public void onDisconnected() {
+            Log.d(APP_TAG, "Health data service is disconnected.");
+            if (!isFinishing()) {
+                mStore.connectService();
+            }
+        }
+    };
+
+    private void showPermissionAlarmDialog() {
+        if (isFinishing()) {
+            return;
+        }
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+        alert.setTitle(R.string.notice)
+                .setMessage(R.string.msg_perm_acquired)
+                .setPositiveButton(R.string.ok, null)
+                .show();
+    }
+
+    private void showConnectionFailureDialog(final HealthConnectionErrorResult error) {
+        if (isFinishing()) {
+            return;
+        }
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        if (error.hasResolution()) {
+            switch (error.getErrorCode()) {
+                case HealthConnectionErrorResult.PLATFORM_NOT_INSTALLED:
+                    alert.setMessage(R.string.msg_req_install);
+                    break;
+                case HealthConnectionErrorResult.OLD_VERSION_PLATFORM:
+                    alert.setMessage(R.string.msg_req_upgrade);
+                    break;
+                case HealthConnectionErrorResult.PLATFORM_DISABLED:
+                    alert.setMessage(R.string.msg_req_enable);
+                    break;
+                case HealthConnectionErrorResult.USER_AGREEMENT_NEEDED:
+                    alert.setMessage(R.string.msg_req_agree);
+                    break;
+                default:
+                    alert.setMessage(R.string.msg_req_available);
+                    break;
+            }
+        } else {
+            alert.setMessage(R.string.msg_conn_not_available);
+        }
+
+        alert.setPositiveButton(R.string.ok, (dialog, id) -> {
+            if (error.hasResolution()) {
+                error.resolve(MainActivity.this);
+            }
+        });
+
+        if (error.hasResolution()) {
+            alert.setNegativeButton(R.string.cancel, null);
+        }
+
+        alert.show();
+    }
+
+    private boolean isPermissionAcquired() {
+        PermissionKey permKey = new PermissionKey(StepCount.HEALTH_DATA_TYPE, PermissionType.READ);
+        HealthPermissionManager pmsManager = new HealthPermissionManager(mStore);
+        try {
+            // Check whether the permissions that this application needs are acquired
+            Map<PermissionKey, Boolean> resultMap = pmsManager.isPermissionAcquired(Collections.singleton(permKey));
+            return !resultMap.containsValue(Boolean.FALSE);
+        } catch (Exception e) {
+            Log.e(APP_TAG, "Permission request fails.", e);
+        }
+        return false;
+    }
+
+    private void requestPermission() {
+        PermissionKey permKey = new PermissionKey(StepCount.HEALTH_DATA_TYPE, PermissionType.READ);
+        HealthPermissionManager pmsManager = new HealthPermissionManager(mStore);
+        try {
+            // Show user permission UI for allowing user to change options
+            pmsManager.requestPermissions(Collections.singleton(permKey), MainActivity.this)
+                    .setResultListener(result -> {
+                        Log.d(APP_TAG, "Permission callback is received.");
+                        Map<PermissionKey, Boolean> resultMap = result.getResultMap();
+
+                        if (resultMap.containsValue(Boolean.FALSE)) {
+                            updateStepCountView("");
+                            showPermissionAlarmDialog();
+                        } else {
+                            // Get the current step count and display it
+                            mReporter.start();
+                        }
+                    });
+        } catch (Exception e) {
+            Log.e(APP_TAG, "Permission setting fails.", e);
+        }
+    }
+
+    private StepCountReporter.StepCountObserver mStepCountObserver = count -> {
+        Log.d(APP_TAG, "Step reported : " + count);
+        updateStepCountView(String.valueOf(count));
+    };
+
+    private void updateStepCountView(final String count) {
+        runOnUiThread(() -> mBinding.editHealthDateValue1.setText(count));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(android.view.MenuItem item) {
+
+        if (item.getItemId() == R.id.connect) {
+            requestPermission();
+        }
+
+        return true;
+    }
+
+
+}
